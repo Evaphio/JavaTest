5aa844d92dd4d891a4569e7f9b52c2b882db9cd0
Evgeniia Kriuchkova <phiophiophio@gmail.com>
2021-09-22 17:39:53 +0300

    First version

---
+++ JavaTest/src/main/java/com/mycompany/javatest/JavaTest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.javatest;
+
+/**
+ *
+ * @author Evgusia
+ */
+public class JavaTest {
+    public static void main(String[] args) {
+    
+    System.out.println("What we see and what we seem are but a dream – a dream within a dream.");
+    }
+    
+}
